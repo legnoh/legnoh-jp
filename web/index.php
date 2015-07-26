23cec512ea3757aaeb8826620890c404be2451f9
Ryoma Kai <legknmj@icloud.com>
2015-07-26 15:14:26 +0900

    first commit

--- web/index.php
+++ web/index.php
@@ -1,20 +1,59 @@
-<?php
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<link href='http://fonts.googleapis.com/css?family=Delius+Unicase:700' rel='stylesheet' type='text/css'>
+	<style>
+		img.bg {
+			min-height: 100%;
+			min-width: 1024px;
+			width: 100%;
+			height: auto;
+			position: fixed;
+			top: 0;
+			left: 0;
+		}
 
-require('../vendor/autoload.php');
+		@media screen and (max-width: 1024px){
+			img.bg {
+			left: 50%;
+			margin-left: -512px; }
+		}
 
-$app = new Silex\Application();
-$app['debug'] = true;
+		div#container {
+			position: relative;
+			font-family: 'Delius Unicase', cursive;
+			font-size: 90pt;
+			color: #5a3700;
+		}
+		footer{
+			font-size: 30pt;
+			position:fixed!important;
+			position:absolute;
+			bottom: 0;
+		}
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+		footer.right{
+			right:0;
+		}
 
-// Our web handlers
+		footer.left{
+			left:0;
+		}
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return 'Hello';
-});
-
-$app->run();
+		a{
+			color: #5a3700;
+		}
+	</style>
+</head>
+<body>
+<img class="bg" src="img/tente.gif">
+<div id="container">
+  UNDER<br>MAINTENANCE
+  <footer class="right">
+  Powered by <a href="http://legnoh.hatenablog.com">legnoh</a><br>
+  Animation by <a href="http://www.tbs.co.jp/anime/hidamari/">✖╹◡╹✖</a>
+  </footer>
+</div>
+</body>
+</html>
